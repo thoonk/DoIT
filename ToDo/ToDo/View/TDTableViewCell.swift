ff3cd9b15c4464c344c76703640fe763ca03b612
thoonk <dkrmrm878@gmail.com>
2020-11-14 15:02:59 +0900

    Add cocoapods

---
+++ ToDo/ToDo/View/TDTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TodoTableViewCell.swift
+//  Todo
+//
+//  Created by 김태훈 on 2020/11/10.
+//
+
+import UIKit
+
+class TDTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    private lazy var dateFormmater: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    func mappingData(_ data: TDItem){
+        titleLabel.text = data.title
+        dateLabel.text = dateFormmater.string(from: data.date)
+    }
+}
