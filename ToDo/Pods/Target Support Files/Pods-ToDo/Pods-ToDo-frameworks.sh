2fc191cc7b9802cadc33680f80af1efa0db655c2
thoonk <dkrmrm878@gmail.com>
2020-12-22 21:45:10 +0900

    Update Pods

--- ToDo/Pods/Target Support Files/Pods-ToDo/Pods-ToDo-frameworks.sh
+++ ToDo/Pods/Target Support Files/Pods-ToDo/Pods-ToDo-frameworks.sh
@@ -175,10 +175,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
 fi
