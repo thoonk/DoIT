dee78142bf185fcac7f37e30e9a419f444af75d0
thoonk <dkrmrm878@gmail.com>
2020-12-24 20:39:44 +0900

    Add Constants

---
+++ ToDo/ToDo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ToDo
+//
+//  Created by 김태훈 on 2020/12/24.
+//
+
+import Foundation
+
+struct C {
+    
+    static let tableCell: String = "tableCell"
+    static let detailFromTable: String = "detailFromTable"
+}
