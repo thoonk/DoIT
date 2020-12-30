80b297e16c42910715d255110ecd74a0742123f4
thoonk <dkrmrm878@gmail.com>
2020-12-30 23:18:48 +0900

    Add Background Color

---
+++ ToDo/ToDo/Others/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  ToDo
+//
+//  Created by 김태훈 on 2020/12/24.
+//
+
+import Foundation
+
+struct C {
+    
+    static let tableCell: String = "tableCell"
+    static let detailFromTable: String = "detailFromTable"
+    
+    struct notiBody {
+        static let startBody: String = "Start Time left 10min !!"
+        static let endBody: String = "End Time left 10min !!"
+    }
+}
