02a48a431080781cfe375418eccaaff3a515637d
thoonk <dkrmrm878@gmail.com>
2021-01-07 23:22:30 +0900

    Set selectionStyle.none

--- DoIT/DoIT/View/DIInfoTableViewCell.swift
+++ DoIT/DoIT/View/DIInfoTableViewCell.swift
@@ -15,7 +15,8 @@ class DIInfoTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.selectionStyle = .none
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
